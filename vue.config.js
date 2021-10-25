752ef94cc34e6de5c741a79a6abf3a3699053f22
maruyamaizumi <maruyama@s-online.co.jp>
2021-10-25 17:35:35 +0900

    3 commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/test_vue' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/test-vue-project' : '/'
 }
